c6180a5c06774bb143f3c39bba679b7833b13553
jason <eofpsh@gmail.com>
2025-11-30 16:32:55 +0900

    cicd: 기본 테스트 코드 비활성화

--- community-BE/src/test/java/com/ktb/community_BE/CommunityBeApplicationTests.java
+++ community-BE/src/test/java/com/ktb/community_BE/CommunityBeApplicationTests.java
@@ -2,6 +2,7 @@ package com.ktb.community_BE;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
 
 @SpringBootTest
 @Disabled
